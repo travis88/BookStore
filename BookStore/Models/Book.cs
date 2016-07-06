da95915e8e34fb6231fc46ecacee14593fdb8e9f
travis88 <hellsballs99@gmail.com>
2016-07-06 19:59:10 +0300

    Initial

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        // ID книги
+        public int Id { get; set; }
+        // Название книги
+        public string Name { get; set; }
+        // Автор книги
+        public string Author { get; set; }
+        // Цена книги
+        public int Price { get; set; }
+    }
+}
---
+++ BookStore/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Purchase
+    {
+        // ID книги
+        public int PurchaseId { get; set; }
+        // Имя и фамилия покупателя
+        public string Person { get; set; }
+        // Адрес покупателя
+        public string Address { get; set; }
+        // ID книги
+        public int BookId { get; set; }
+        // Дата покупки
+        public DateTime Date { get; set; }
+    }
+}
